baf68c6dbaea721dd2531b7933c04a45b45afa49
PrincessOfEvil <laz2727@gmail.com>
2022-06-29 10:32:51 +0500

    laser boolet test

---
+++ Source/SPADE/SPADE/Bullet_Laser.cs
@@ -0,0 +1,68 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+using Verse.Sound;
+
+namespace SPADE
+	{
+	public class Bullet_Laser : Bullet
+        {
+		protected bool impacted = false;
+
+		protected Action<Bullet> ImpactSomething = AccessTools.MethodDelegate<Action<Bullet>>(AccessTools.Method(typeof(Bullet), "ImpactSomething"));
+
+		private Material mat;
+
+		public override Vector3 ExactPosition => destination.Yto0() + Vector3.up * def.Altitude;
+
+		public override void Draw()
+			{
+			if (mat == null)
+				mat =  def.graphic.MatSingle;
+			mat.color = mat.color.ToTransparent(GenMath.InverseParabola(def.graphicData.drawSize.y * ticksToImpact / def.projectile.speed));
+			GenDraw.DrawLineBetween(origin, destination, mat, def.graphicData.drawSize.x);
+			base.Comps_PostDraw();
+			}
+
+		public override void Tick()
+			{
+			if (!impacted)
+				{
+				impacted = true;
+				Position = DestinationCell;
+				ticksToImpact = Mathf.CeilToInt(def.projectile.speed);
+				ImpactSomething(this);
+				}
+			this.ticksToImpact--;
+			if (!this.ExactPosition.InBounds(base.Map))
+				{
+				ticksToImpact++;
+				Position = ExactPosition.ToIntVec3();
+				base.Destroy();
+				return;
+				}
+			if (ticksToImpact == 60 && Find.TickManager.CurTimeSpeed == TimeSpeed.Normal && def.projectile.soundImpactAnticipate != null)
+				{
+				def.projectile.soundImpactAnticipate.PlayOneShot(this);
+				}
+			if (ticksToImpact <= 0)
+				{
+				if (DestinationCell.InBounds(Map))
+					{
+					Position = DestinationCell;
+					}
+				base.Destroy();
+				}
+			}
+		public override void Destroy(DestroyMode mode = DestroyMode.Vanish)
+			{
+			//jk
+			}
+		}
+    }
